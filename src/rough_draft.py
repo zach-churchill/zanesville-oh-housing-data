d0f40a5c6b202814e8ba6f651edf07ba7b34cd85
zach-churchill <zach.churchill2016@gmail.com>
2017-12-20 21:59:08 -0500

    Moved all code to one file and created more functions
    - Moved all of the code from separate Python scripts to one Python
    script for initial development.
    - Updated setup_database() function from database.py to include error
    checking, and to give the option to use the function as a means to
    opening the connection to the database as well.
    - Created functions to handle errors in a simple way, prepare a Selenium
    webdriver for the advanced search page, store the scraped parcel
    numbers, and a simple shell function to return a fake parcel number.
    - Lastly, I created a simple, visual integration test in the main
    function to check database connection/insertion, along with connecting
    the the auditor's website.

--- src/rough_draft.py
+++ src/rough_draft.py
@@ -1,32 +1,195 @@
 import os
-from utility import find_project_path
-from database import setup_database
+import sys
+import json
+import sqlite3
+from pathlib import Path
+import selenium
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
-def main():
-    project_name = 'zanesville-oh-housing-data'
-    project_path = find_project_path(project_name)
+URLS = {
+        'advanced-search-results': "http://www.muskingumcountyauditor.org/Results.aspx?SearchType=Advanced&Criteria=20g%2byYTTdkDKRrEbpO1sLV9b36Zp5GCYSiEbzYPtPXU%3d",
+        'parcel-id-data-fmt': "http://muskingumcountyauditor.org/Data.aspx?ParcelID={parcel_id}"
+    }
 
-    # With the project path, set up paths to necessary folders
-    raw_data_path = os.path.join(project_path, 'data', 'raw')
-    resources_path = os.path.join(project_path, 'resources')
+TABLE_NAME = 'parcel_numbers'
+TABLE_SCHEMA = 'CREATE TABLE {} (number TEXT UNIQUE)'.format(TABLE_NAME)
 
-    # Set up the SQLite database file for the parcel numbers
-    database_name = 'parcel.sqlite'
-    database_path = os.path.join(raw_data_path, database_name)
-    table_schema = 'CREATE TABLE parcel_numbers (number TEXT UNIQUE)'
+def handle_error(err):
+    """Prints error to the screen, with '[ERROR]' prepended to the message.
+    Then, it exits the program.
 
-    if os.path.exists(database_path):
-        os.remove(database_path)
-        parcel_conn = setup_database(database_path, table_schema)
+    Parameters
+    ----------
+    err (exception) : Inherited exception class.
+
+    """
+    print('[ERROR] {}'.format(err))
+    sys.exit()
+
+def find_project_path(project_name):
+    """Returns the full path for the project given that the project name is
+    exactly how it is in the user's files and is in the path tree from the
+    directory for which this function is called.
+
+    Example
+    --------
+    If the project name is 'Project1', and the current directory that this
+    function is '/home/zach/Documents/Project1/src/module1' then this function
+    will return '/home/zach/Documents/Project1'.
+
+    Parameters
+    ----------
+    project_name (str) : Name of the project, exactly how it is for the folder.
+
+    Returns
+    -------
+    project_path (str) : The absolute path for the project if found; otherwise,
+        error is printed to screen and program exited.
+
+    """
+    curr_dir = Path(os.getcwd())
+    path_parts = curr_dir.parts
+
+    try:
+        project_part_index = path_parts.index(project_name)
+    except ValueError as err:
+        handle_error(err)
     else:
-        parcel_conn = setup_database(database_path, table_schema)
+        project_path = os.path.join(*path_parts[:project_part_index+1])
 
-    # Simple test
+    return project_path
+
+def setup_database(database_file_path):
+    """Given the path, a SQLite is created with a simple one column table if
+    one doesn't already exist; otherwise, it simply connects to the existing
+    database file. Returns the sqlite3 connection object.
+
+    Parameters
+    ----------
+    database_file_path (str) : Path string pointing to the SQLite database file.
+    table_schema (str) : SQL string to set up database table.
+
+    Returns
+    -------
+    connection_obj (sqlite3.Connection) : Connection object to the database.
+
+    """
+    #if not os.path.exists(database_file_path):
+    connection_obj = sqlite3.connect(database_file_path)
+    cursor = connection_obj.cursor()
+    try:
+        cursor.execute(TABLE_SCHEMA)
+    except sqlite3.OperationalError as err:
+        if str(err) == 'table {} already exists'.format(TABLE_NAME):
+            pass
+        else:
+            handle_error(err)
+    else:
+        connection_obj.commit()
+    finally:
+        cursor.close()
+
+    return connection_obj
+
+def prepare_search_page_webdriver(project_path):
+    """Prepares the search page session for retrieving the parcel numbers
+    for the City of Zanesville.
+
+    Parameters
+    ----------
+    project_path (str) : Path of the Git project folder.
+
+    Returns
+    -------
+    search_page_driver (selenium.webdriver) : Webdriver using PhantomJS, and
+        is set to the advanced search results for the auditor's website for
+        the City of Zanesville.
+
+    """
+    # Create a link to the PhantomJS API file and set up a webdriver
+    phantomjs_path = os.path.join(project_path, 'resources', 'phantomjs')
+    search_page_driver = webdriver.PhantomJS(executable_path=phantomjs_path)
+
+    # Navigate to the page with the parcel numbers
+    search_page_driver.get(URLS['advanced-search-results'])
+
+    # If there is a disclaimer, use the necessary JavaScript to press
+    # the 'Accept' button (read if you have not done so first)
+    button_css_id = 'ContentPlaceHolder1_btnDisclaimerAccept'
+    js_fmt_code = "document.getElementById('{}').click();"
+    js_code = js_fmt_code.format(button_css_id)
+    try:
+        search_page_driver.execute_script(js_code)
+    except Exception as err:
+        handle_error(err)
+
+    # Return the prepared webdriver object
+    return search_page_driver
+
+def store_parcel_numbers(database_path, parcel_numbers):
+    """Stores the scraped parcel numbers into a SQLite database, where the
+    table consists of just one column for unique parcel numbers.
+
+    Parameters
+    ----------
+    database_path (str) : Path to the SQLite database
+    parcel_numbers (list) : List of strings of parcel numbers scraped from
+        the advanced search webpage.
+
+    Returns
+    -------
+    database_path (str) : Path to the SQLite database file.
+
+    """
+    # Set up the SQLite database file for the parcel numbers
+    parcel_conn = setup_database(database_path)
+
+    # Insert parcel numbers iteratively so that we can catch when a
+    # parcel number is not unique and quietly dismiss
+    fmt_insert_stmt = "INSERT INTO parcel_numbers VALUES (?)"
     parcel_cursor = parcel_conn.cursor()
-    parcel_cursor.execute("INSERT INTO parcel_numbers VALUES ('1234')")
+    for parcel_number in parcel_numbers:
+        try:
+            parcel_cursor.execute(fmt_insert_stmt, parcel_numbers)
+        except sqlite3.IntegrityError:
+            pass
+
+    # Commit the inserts to the database
     parcel_conn.commit()
 
-    parcel_cursor.execute('SELECT * FROM parcel_numbers')
+    # Tear down
+    parcel_cursor.close()
+    parcel_conn.close()
+
+    return database_path
+
+def scrape_parcel_numbers(search_page_driver):
+    """Shell function to understand the process."""
+    parcel_numbers = ['1234']
+    return parcel_numbers
+
+def main():
+    # Set up
+    project_name = 'zanesville-oh-housing-data'
+    project_path = find_project_path(project_name)
+    raw_data_path = os.path.join(project_path, 'data', 'raw')
+    database_name = 'parcel.sqlite'
+    database_path = os.path.join(raw_data_path, database_name)
+
+    # Setup the webdriver to the search page
+    search_page_driver = prepare_search_page_webdriver(project_path)
+
+    # Scrape as many parcel numbers as we can
+    parcel_numbers = scrape_parcel_numbers(search_page_driver)
+
+    # Store the results in a SQLite database
+    database_path = store_parcel_numbers(database_path, parcel_numbers)
+
+    # Test
+    parcel_conn = setup_database(database_path)
+    parcel_cursor = parcel_conn.cursor()
+    parcel_cursor.execute('SELECT * FROM {}'.format(TABLE_NAME))
     print(parcel_cursor.fetchone())
 
     # Tear down
